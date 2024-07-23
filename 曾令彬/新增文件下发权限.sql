d668f0ac1d7b7409beb109bc1035d416b7c026a0
zlb <1101246047@qq.com>
2024-07-23 18:30:35 +0800

    first commit

---
+++ 曾令彬/新增文件下发权限.sql
@@ -0,0 +1,4 @@
+INSERT INTO `sys_role_menu` (`role_id`, `menu_id`) VALUES ('1', '2ebe6661553b4b8eb7d0e9ecd6f4a3d9');
+INSERT INTO `sys_role_menu` (`role_id`, `menu_id`) VALUES ('1', 'f541c2c681b746129918c9996630c064');
+INSERT INTO `sys_role_menu` (`role_id`, `menu_id`) VALUES ('90b5affb3bfa4a37b00545ebf0938125', '2ebe6661553b4b8eb7d0e9ecd6f4a3d9');
+INSERT INTO `sys_role_menu` (`role_id`, `menu_id`) VALUES ('90b5affb3bfa4a37b00545ebf0938125', 'f541c2c681b746129918c9996630c064');
